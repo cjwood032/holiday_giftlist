9148c01ebe05888462786a93e85a3058172fe786
cjwood032 <cjwood032@gmail.com>
2018-11-22 10:49:36 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+
+  resources :users, only: [:new, :create, :show] do
+    resources :friends do
+      resources :gifts
+      get 'gifts/:id/next', to: 'gifts#next'
+    end
+    get 'friends/:id/next', to: 'friends#next'
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
