e0617c9d47f3ef742c1e3dcf1008b8a836e55764
cjwood032 <cjwood032@gmail.com>
2018-11-22 10:27:27 -0500

    added CRUD to controller

---
+++ app/controllers/friends_controller.rb
@@ -0,0 +1,28 @@
+class FriendsController < ApplicationController
+    def index
+        @friends=Friend.ApplicationController
+        render json: @friends, status:200
+    end
+    def show
+        @friend = Friend.find(params[:id])
+        render json: @friend, status: 200
+    end
+    def create
+        @friend = Friend.create(friend_params)
+        render json: @friend, status: 200
+    end
+    def update
+        @friend = Friend.find(params[:id])
+        @friend.update(friend_params)
+        render json: @friend, status: 200
+    end
+    def destroy
+        @friend = Friend.find(params[:id])
+        @friend.delete
+        render json: {friendId: @friend.id}
+    end
+    private
+    def friend_params
+        params.require(:friend).permit(:body) #double check after creating form
+    end
+end
