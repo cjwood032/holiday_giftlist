102163d0f0ac4ff1df6329dde4bc18948f0193b1
cjwood032 <cjwood032@gmail.com>
2018-11-23 13:58:46 -0500

    session and static pages controller init

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+    def destroy
+        session[:user_id] = nil
+        redirect_to root_url
+      end
+    
+      def new
+        @user = User.new
+        @users = User.all
+      end
+    
+      def create
+        @user = User.find_or_create_by(auth_hash)
+        current_user = @user
+        if @user && @user.authenticate(params[:password])
+          session[:user_id] = @user.id
+          redirect_to user_path(@user), notice: "Welcome back!"
+        else
+          redirect_to signin_path, notice: "Name or password is incorrect"
+        end
+      end
+
+
+      private
+      
+      def auth
+        request.env['omniauth.auth']
+      end
+      
+      def auth_hash
+        request.env['omniauth.auth']
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+  root "static_pages#home"
   get "/login" => "sessions#new"
   post "/login" => "sessions#create"
   get "/logout" => "sessions#destroy"
