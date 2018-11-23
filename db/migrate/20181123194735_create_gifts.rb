3b605265ed1367c365254663287b8f6552462ef8
cjwood032 <cjwood032@gmail.com>
2018-11-23 15:05:19 -0500

    added tables

--- db/migrate/20181123194735_create_gifts.rb
+++ db/migrate/20181123194735_create_gifts.rb
@@ -1,7 +1,13 @@
 class CreateGifts < ActiveRecord::Migration[5.2]
   def change
     create_table :gifts do |t|
-
+      t.integer :user_id
+      t.integer :friend_id
+      t.string :name
+      t.string :link
+      t.integer :price
+      t.string :status
+      t.integer :quantity
       t.timestamps
     end
   end
