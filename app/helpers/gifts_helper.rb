f00fdbe3949dae3d10df33713daee6332a9d975d
cjwood032 <cjwood032@gmail.com>
2018-12-02 15:05:52 -0500

    working on friend selection gifts

--- app/helpers/gifts_helper.rb
+++ app/helpers/gifts_helper.rb
@@ -1,15 +1,19 @@
 module GiftsHelper
     def user_gifts_table(user)
         if @friend
-            render partial: "gifts/ftable", locals: {user: @user, friends: user.friends, friend: @friend, gifts: user.gifts}
-        else
-            binding.pry
+            render partial: "gifts/table", locals: {user: @user, friends: user.friends, friend: @friend, gifts: user.gifts}
+        else 
             if user.gifts.size == 0
                 content_tag(:p, "You haven't found any gifts".html_safe)
-              else
-                render partial: "gifts/table", locals: {user: @user, friends: user.friends, gifts: user.gifts}
-              end
+            else
+                user.friends.each do |f|
+                    render partial: "gifts/table", locals: {user: @user, friends: user.friends, friend: f, gifts: user.gifts}
+                end
+            end
         end
-        
       end
+
+    def friend_selection(user)
+        render partial: "gifts/selection", locals: {user: @user, friends: user.friends, gifts: user.gifts}
+    end
 end
