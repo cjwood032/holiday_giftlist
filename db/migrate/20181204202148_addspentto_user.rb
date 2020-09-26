d5b86767cc5266e36769d39c1255b834c28c7b7d
cjwood032 <cjwood032@gmail.com>
2020-09-26 10:41:12 -0400

    heroku postgres

--- db/migrate/20181204202148_addspentto_user.rb
+++
@@ -1,6 +0,0 @@
-class AddamountspenttoUser < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :amount_spent, :integer
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_12_04_202630) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "friend_gifts", force: :cascade do |t|
     t.integer "friend_id"
     t.integer "gift_id"
